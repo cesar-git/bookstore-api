e823ce8199c493ce1f1e028f2a7cc8605e550d02
cesar-git <cesarsax@gmail.com>
2022-07-31 00:11:21 -0300

    commit inicial

---
+++ src/test/java/com/cesar/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
