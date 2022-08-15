e7058715e91e08561978ffbf771d2784fb003f16
cesar-git <cesarsax@gmail.com>
2022-08-15 18:31:28 -0300

    findById para Categoria

--- src/main/java/com/cesar/bookstore/service/DBService.java
+++ src/main/java/com/cesar/bookstore/service/DBService.java
@@ -30,13 +30,15 @@ public class DBService {
 		Livro l3 = new Livro(null, "The Time Machine", "H.G. Wells", "Lorem ipsum", cat2);
 		Livro l4 = new Livro(null, "The War of the Worlds", "H.G. Wells", "Lorem ipsum", cat2);
 		Livro l5 = new Livro(null, "I, Robot", "Isaac Asimov", "Lorem ipsum", cat2);
-		Livro l6 = new Livro(null, "Capitães de Areia", "Jorge Amado", "Lorem ipsum", cat3);
+		Livro l6 = new Livro(null, "Jimi Hendrix Anjo Insjustiçado", "Lorem ipsum", "Biografia", cat3);
+		Livro l7 = new Livro(null, "Capitães de Areia", "Jorge Amado", "Lorem ipsum", cat4);
 				
 		cat1.getLivros().addAll(Arrays.asList(l1, l2));
 		cat2.getLivros().addAll(Arrays.asList(l3, l4, l5));
 		cat3.getLivros().addAll(Arrays.asList(l6));
+		cat4.getLivros().addAll(Arrays.asList(l7));
 		
 		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4));
-		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5, l6));
+		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5, l6, l7));
 	}
 }
