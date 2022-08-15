dce6bae74d245731de49653d34aaa9c47fcc17bc
cesar-git <cesarsax@gmail.com>
2022-08-15 19:48:18 -0300

    excessao personalizada para objeto não encontrado

--- src/main/java/com/cesar/bookstore/service/CategoriaService.java
+++ src/main/java/com/cesar/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.cesar.bookstore.domain.Categoria;
 import com.cesar.bookstore.repositories.CategoriaRepository;
+import com.cesar.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,9 @@ public class CategoriaService {
 	public Categoria findById(Integer id) {
 		
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> 
+		new ObjectNotFoundException("Objeto não econtrado! Id: " + id + 
+				", Tipo: " + Categoria.class.getName()));
 	}
 
 }
