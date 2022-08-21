4a50a068d2f19365badf1bb00597647ea06ec9bb
cesar-git <cesarsax@gmail.com>
2022-08-20 22:10:46 -0300

    Excecao personalizada para DELETE Categoria

--- src/main/java/com/cesar/bookstore/service/CategoriaService.java
+++ src/main/java/com/cesar/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.cesar.bookstore.domain.Categoria;
@@ -38,8 +39,13 @@ public class CategoriaService {
 	}
 
 	public void delete(Integer id) {
-		findById(id);
-		repository.deleteById(id);
+		findById(id);		
+		try {
+			repository.deleteById(id);			
+		} catch (DataIntegrityViolationException e) {
+			throw new com.cesar.bookstore.service.exceptions.DataIntegrityViolationException("Categoria não pode ser deletada! Possui livros associados");
+		}
+		
 	}
 
 }
